7023cf192bb9b2deadd5a713ddf62623f11f8761
Nellie <nelliostankova04@gmail.com>
2020-11-22 19:50:01 +0300

    Working with submodule

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -19,21 +21,13 @@ project "ImGui"
 		"imgui_demo.cpp"
 	}
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "system:windows"
+            systemversion "latest"
+            
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+        
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
